27c3bab39990d09a5ce2b6e355ebb0a52ec7e5d8
张昌盛 <zcs@192.168.0.103>
2017-10-18 22:22:42 +0800

    add file

---
+++ stock/stock/spiders/stock_spider.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# File Name: stock_spider.py
+# Author: Changsheng Zhang
+# mail: zhangcsxx@gmail.com
+# Created Time: Wed Oct 18 20:35:57 2017
+
+#########################################################################
+
+import scrapy
+from scrapy.selector import Selector
+from stock.items import StockItem
+import logging
+
+class StockSpider(scrapy.Spider):
+
+    name = 'stock'
+    allowed_domains = ["hkexnews.hk"]
+    start_urls = ['http://www.hkexnews.hk/sdw/search/mutualmarket.aspx?t=hk']
+
+    def parse(self,response):
+        
+        
+        f = open("log.csv","w")
+        for sel in response.xpath("//tr").extract():
+            
+            item = StockItem() 
+            info = Selector(text = sel).xpath("//td/text()").extract()
+            if len(info) !=4:
+                continue
+
+            a = info[0].split("\r\n")[1]
+            b = info[1].split("\r\n")[1]
+            c = info[2].split("\r\n")[1]
+            d = info[3].split("\r\n")[1]
+            
+            f.write(a+";"+b+";"+c+";"+d+"\n")
+        f.close()
