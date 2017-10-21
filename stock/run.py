f9a4473f032e779a29a38f50acde02ed5e58263d
ChangshengZhang <zhangcsxx@gmail.com>
2017-10-21 14:18:11 +0800

    add run.py

---
+++ stock/run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# File Name: run.py
+# Author: Changsheng Zhang
+# mail: zhangcsxx@gmail.com
+
+#########################################################################
+
+import os
+
+for ii in xrange(1000):
+
+    print "begin run\n"
+    os.system("scrapy crawl stock")
+
+    print "done, begin sleep"
+    a = 3600*24
+    time.sleep(a)
